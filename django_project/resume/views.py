ac129316bc26803cda05c39a08ffc0a5bb04548d
T-sosthenese <sosthenetimi94@gmail.com>
2023-12-26 23:56:18 +0300

    Worked on the resume and company apps effectively

--- django_project/resume/views.py
+++ django_project/resume/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Resume
+from users.models import User
+from .forms import UpdateResumeForm
 
-# Create your views here.
+
+def update_resume(request):
+    resume = Resume.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = UpdateResumeForm(request.POST, instance=resume)
+        if form.is_valid():
+            var = form.save(commit=False)
+            user = User.objects.get(pk=request.user.id)
+            user.has_resume = True
+            user.save()
+            var.save()
+            messages.info(request, 'Your resume info has been updated.')
+            return redirect('dashboard')
+        else:
+            messages.warning(request, 'Something went wrong.')
+    else:
+        form = UpdateResumeForm(instance=resume)
+        context = {'form':form}
+        return render(request, 'resume/update_resume.html', context)
+
+def resume_details(request, pk):
+    resume = Resume.objects.get(pk=pk)
+    context = {'resume':resume}
+    return render(request, 'resume/resume_details.html', context)
